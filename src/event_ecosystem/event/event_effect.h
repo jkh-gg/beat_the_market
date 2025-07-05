1de70ade684d82b42084ed89564a907c0d106910
jkhgg <jkhgg@proton.me>
2025-07-05 03:05:35 +0000

    create event effect

---
+++ src/event_ecosystem/event/event_effect.h
@@ -0,0 +1,28 @@
+#ifndef EVENTEFFECT_H
+#define EVENTEFFECT_H
+
+#include <string>
+#include <vector>
+
+#include "common/region.h"
+#include "common/sector.h"
+
+struct EventEffect {
+   private:
+    std::vector<Sector> targetSectors;
+    std::vector<Region> targetRegions;
+    std::vector<std::string> targetTickers;
+
+    double priceBiasInitial;
+    double priceBiasDecayRate;
+
+    double profitBiasStart;
+    double profitBiasEnd;
+
+    double volatilityChangeInitial;
+    double volatilityChangeEnd;
+
+   public:
+};
+
+#endif
