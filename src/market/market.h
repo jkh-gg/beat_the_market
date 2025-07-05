1502a90c335f9164c780625fe68a42acb98d6c94
jkhgg <jkhgg@proton.me>
2025-07-05 13:23:47 +0000

    market now holds an orchestrator

--- src/market/market.h
+++ src/market/market.h
@@ -5,26 +5,26 @@
 #include <string>
 #include <vector>
 
+#include "../event_ecosystem/event_orchestrator/event_orchestrator.h"
 #include "../security/security.h"
 
 class Market {
    private:
     std::default_random_engine &rng;
 
-    std::vector<Security> listings;
     double currentSofr;
-
-    std::string eo;  // TODO- Will be event orchestrator
-
+    std::vector<Security> listings;
     std::vector<double> interestRateHistory;
+
+    EventOrchestrator eo;
 
    public:
     Market(std::default_random_engine &rng, double currentSofr);
 
-    std::vector<Security> getListings() const { return listings; }
     double getSofr() const { return currentSofr; }
-    std::string getEventOrchestrator() const { return eo; }
+    std::vector<Security> getListings() const { return listings; }
     std::vector<double> getRateHistory() const { return interestRateHistory; }
+    EventOrchestrator getEventOrchestrator() const { return eo; }
 };
 
 #endif
