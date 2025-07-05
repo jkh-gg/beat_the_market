647a6fcf252a48e00efc733278af739b86790665
jkhgg <jkhgg@proton.me>
2025-07-05 13:23:14 +0000

    proper use of references

--- src/event_ecosystem/event_orchestrator/event_orchestrator.cpp
+++ src/event_ecosystem/event_orchestrator/event_orchestrator.cpp
@@ -1,10 +1,6 @@
 #include "event_orchestrator.h"
 
-std::vector<Event> EventOrchestrator::getFutureEventsAt(int quarter) const {
-    try {
-        std::vector<Event> events = quarterToEventMap.at(quarter);
-        return events;
-    } catch (const std::exception& e) {
-        return std::vector<Event>();
-    }
+void EventOrchestrator::addFutureEventAt(int quarter, Event newEvent) {
+    std::vector<Event>& eventsAt = getFutureEventsAt(quarter);
+    eventsAt.push_back(newEvent);
 }
--- src/event_ecosystem/event_orchestrator/event_orchestrator.h
+++ src/event_ecosystem/event_orchestrator/event_orchestrator.h
@@ -9,12 +9,15 @@
 class EventOrchestrator {
    private:
     std::map<int, std::vector<Event>> quarterToEventMap;
-
     std::vector<Event> activeEvents;
 
    public:
-    std::vector<Event> getFutureEventsAt(int quarter) const;
+    std::vector<Event>& getFutureEventsAt(int quarter) {
+        return quarterToEventMap[quarter];
+    }
     std::vector<Event> getActiveEvents() const { return activeEvents; }
+
+    void addFutureEventAt(int quarter, Event newEvent);
 };
 
 #endif
